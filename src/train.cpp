064ba1784491294c8a6549ea90cc9b7ffac04df1
perchig <ivanslepko11@gmail.com>
2025-05-15 19:46:28 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -38,6 +38,7 @@ int Train::getLength() {
   Car* currentCar = first;
   int totalCars = 1;
   bool originalLight = currentCar->light;
+
   currentCar->light = !originalLight;
   countOp++;
 
@@ -48,7 +49,6 @@ int Train::getLength() {
     currentCar->light = !originalLight;
     countOp++;
   }
-
   currentCar = first;
   for (int i = 0; i < totalCars; i++) {
     currentCar->light = originalLight;
